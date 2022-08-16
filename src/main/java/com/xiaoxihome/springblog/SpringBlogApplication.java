0192e0797b9ba8263ec3a066e79190fe47db71e1
Xiaoxi <yxx0827@gmail.com>
2022-08-15 21:46:19 -0400

    update

--- src/main/java/com/xiaoxihome/springblog/SpringBlogApplication.java
+++ src/main/java/com/xiaoxihome/springblog/SpringBlogApplication.java
@@ -1,13 +1,36 @@
 package com.xiaoxihome.springblog;
 
+import com.xiaoxihome.springblog.domain.Role;
+import com.xiaoxihome.springblog.domain.User;
+import com.xiaoxihome.springblog.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class SpringBlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBlogApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "USER"));
+			userService.saveRole(new Role(null, "ADMIN"));
+
+			userService.saveUser(new User(null, "Aaa", "aaa@test.com", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Bbb", "bbb@test.com", "1234", new ArrayList<>()));
+
+			userService.addRoleToUser("Aaa", "USER");
+			userService.addRoleToUser("Bbb", "USER");
+
+			userService.addRoleToUser("Aaa", "ADMIN");
+		};
 	}
 
 }
--- src/main/java/com/xiaoxihome/springblog/api/UserResource.java
+++ src/main/java/com/xiaoxihome/springblog/api/UserResource.java
@@ -1,13 +1,19 @@
 package com.xiaoxihome.springblog.api;
 
+import com.xiaoxihome.springblog.domain.Role;
 import com.xiaoxihome.springblog.domain.User;
 import com.xiaoxihome.springblog.service.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @Controller
@@ -20,4 +26,22 @@ public class UserResource {
   public ResponseEntity<List<User>>getUsers() {
     return ResponseEntity.ok().body(userService.getUsers());
   }
+
+  @PostMapping("/user/save")
+  public ResponseEntity<User>saveUser(@RequestBody User user) {
+    URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+    return ResponseEntity.created(uri).body(userService.saveUser(user));
+  }
+
+  @PostMapping("/role/addRoleToUser")
+  public ResponseEntity<Role>saveRole(@RequestBody RoleToUserForm form) {
+    userService.addRoleToUser(form.getUsername(), form.getRoleName());
+    return ResponseEntity.ok().build();
+  }
+}
+
+@Data
+class RoleToUserForm {
+  private String username;
+  private String roleName;
 }
